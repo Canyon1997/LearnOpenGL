4552a6de805532ee63f4540c6df2b6fe35a72235
Canyon Davidson <canyon.davidson@gmail.com>
2025-02-05 22:29:31 -0500

    setup for lesson

--- OpenGLSections/Lighting/BasicLighting/BasicLighting/main.cpp
+++ OpenGLSections/Lighting/BasicLighting/BasicLighting/main.cpp
@@ -10,12 +10,23 @@
 
 //--------------------------------------------------------------
 
+// Window size
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+// delta time
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// Camera
 glm::vec3 camPos(0.0f, 0.0f, 3.0f);
 glm::vec3 camFwd(0.0f, 0.0f, -1.0f);
 glm::vec3 camUp(0.0f, 1.0f, 0.0f);
 Camera mainCamera(camPos, camFwd, camUp);
 
 
+// light source
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 //--------------------------------------------------------------
 
@@ -46,8 +57,147 @@ void mainCam_scroll_wrapper(GLFWwindow* window, double xOffset, double yOffset);
 
 int main()
 {
-	
+    GLFWwindow* window = InitializeOpenGL("BasicLighting", WINDOW_WIDTH, WINDOW_HEIGHT);
+    Shader cubeShader = Shader("Shaders//CubeVertex.glsl", "Shaders//CubeFragment.glsl");
+    Shader lightSourceShader = Shader("Shaders//LightSourceVertex.glsl", "Shaders//LightSourceFragment.glsl");
 
+    glfwSetCursorPosCallback(window, mainCam_cursor_wrapper);
+    glfwSetScrollCallback(window, mainCam_scroll_wrapper);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // Vertices to create a cube
+    float cubeVertices[] = {
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    -0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f
+    };
+
+    unsigned int cubeVBO;
+    unsigned int cubeVAO;
+
+    glGenBuffers(1, &cubeVBO);
+    glGenVertexArrays(1, &cubeVAO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+  
+    unsigned int lightSourceVAO;
+
+    glGenVertexArrays(1, &lightSourceVAO);
+    glBindVertexArray(lightSourceVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // handle delta time
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // user input & delta speed adjustments
+        float origCamSpeed = mainCamera.GetCameraSpeed();
+        float deltaCamSpeed = origCamSpeed * deltaTime;
+        mainCamera.SetCameraSpeed(deltaCamSpeed);
+        ProcessInput(window);
+        mainCamera.SetCameraSpeed(origCamSpeed);
+
+        // render
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // cube
+        cubeShader.Use();
+        cubeShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        cubeShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+
+        mainCamera.GenerateProjectionMatrix(WINDOW_WIDTH, WINDOW_HEIGHT, 0.1f, 100.0f);
+        int cubeProj = glGetUniformLocation(cubeShader.ID, "proj");
+        glUniformMatrix4fv(cubeProj, 1, GL_FALSE, glm::value_ptr(mainCamera.GetProjMatrix()));
+
+        mainCamera.GenerateViewMatrix();
+        int cubeView = glGetUniformLocation(cubeShader.ID, "view");
+        glUniformMatrix4fv(cubeView, 1, GL_FALSE, glm::value_ptr(mainCamera.GetViewMatrix()));
+
+        glm::mat4 model = glm::mat4(1.0f);
+        int cubeModel = glGetUniformLocation(cubeShader.ID, "model");
+        glUniformMatrix4fv(cubeModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+        //light source
+        lightSourceShader.Use();
+
+        int lightProj = glGetUniformLocation(lightSourceShader.ID, "proj");
+        glUniformMatrix4fv(lightProj, 1, GL_FALSE, glm::value_ptr(mainCamera.GetProjMatrix()));
+
+        int lightView = glGetUniformLocation(lightSourceShader.ID, "view");
+        glUniformMatrix4fv(lightView, 1, GL_FALSE, glm::value_ptr(mainCamera.GetViewMatrix()));
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f));
+
+        int lightModel = glGetUniformLocation(lightSourceShader.ID, "model");
+        glUniformMatrix4fv(lightModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindVertexArray(lightSourceVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightSourceVAO);
+    glDeleteBuffers(1, &cubeVBO);
+
+    glfwTerminate();
 	return 0;
 }
 
