79a960971f28814cced94f0e3bec19c87e98b145
Canyon <canyon.davidson@gmail.com>
2026-03-11 01:21:20 -0400

    removed windows library links that glfw already declares. Added vertex data for a triangle, created a vertex buffer linked to a vertex array object. Created temp glsl shaders and linked them to a shader program (class will be made for this later). Added input on the window to close if user presses escape screen. Set the color of the screen to a greenish color. Playing around with the modern use of 'typedef' to organize different buffers properly, starting with the vertex buffer object (VBO).

--- DaGrandRenderer/DaGrandRenderer/Source/main.cpp
+++ DaGrandRenderer/DaGrandRenderer/Source/main.cpp
@@ -7,11 +7,20 @@
 constexpr int WINDOW_WIDTH = 800;
 constexpr int WINDOW_HEIGHT = 600;
 
+// vertex buffer object
+using VBO = unsigned int;
 
+// init functions
 bool initGlfw();
 bool initGlad();
+
+// window functions
 GLFWwindow* createGlfwWindow(int width, int height, const std::string& windowName);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// input
+void ProcessInput(GLFWwindow* currentWindow);
+
 
 int main(int argc, char* argv[])
 {
@@ -31,15 +40,99 @@ int main(int argc, char* argv[])
         glViewport(0, 0, width, height);
     });
 
+    const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+    unsigned int vertexShader;
+
+    const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+    unsigned int fragmentShader;
+
+    float objVertexData[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    VBO buf;
+    glGenBuffers(1, &buf);
+    glBindBuffer(GL_ARRAY_BUFFER, buf);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(objVertexData), objVertexData, GL_STATIC_DRAW);
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "Error, vertex shader failed to compile\n" << infoLog << std::endl;
+        return -1;
+    }
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "Error, fragment shader failed to compile\n" << infoLog << std::endl;
+        return -1;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "Error, shader program failed to compile\n" << infoLog << std::endl;
+        return -1;
+    }
+
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     while (!glfwWindowShouldClose(mainWindow))
     {
+        // input
+        ProcessInput(mainWindow);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check/call events, swap buffers, etc.
         glfwSwapBuffers(mainWindow);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+
+void ProcessInput(GLFWwindow* currentWindow)
+{
+    if (glfwGetKey(currentWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(currentWindow, GLFW_TRUE);
+    }
 }
 
 bool initGlfw()
